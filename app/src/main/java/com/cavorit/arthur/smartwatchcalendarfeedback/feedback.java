31a65d10c39647ff5aa1135c97e78c81da776f6d
ReallyRad <arthur.pointeau@gmail.com>
2014-10-22 00:20:33 +0200

    added android developper calendar lookup code

--- app/src/main/java/com/cavorit/arthur/smartwatchcalendarfeedback/feedback.java
+++ app/src/main/java/com/cavorit/arthur/smartwatchcalendarfeedback/feedback.java
@@ -5,6 +5,7 @@ import android.provider.CalendarContract.Calendars;
 import android.database.Cursor;
 import android.content.ContentResolver;
 import android.net.Uri;
+import android.accounts.AbstractAccountAuthenticator;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,7 +17,7 @@ public class feedback extends PApplet {
     //https://developer.android.com/guide/topics/providers/calendar-provider.html
     // Projection array. Creating indices for this array instead of doing
     // dynamic lookups improves performance.
-    public static final String[] EVENT_PROJECTION = new String[] {
+    public static final String[] EVENT_PROJECTION = new String[]{
             Calendars._ID,                           // 0
             Calendars.ACCOUNT_NAME,                  // 1
             Calendars.CALENDAR_DISPLAY_NAME,         // 2
@@ -31,6 +32,15 @@ public class feedback extends PApplet {
 
     public void setup() {
         // Run query
+
+    }
+
+    public void draw() {
+        background(255, 155, 155);
+        for (int i = 1; i < EVENT_PROJECTION.length; i++) {
+            text(EVENT_PROJECTION[i],20 * i, 10);
+        }
+
         Cursor cur = null;
         ContentResolver cr = getContentResolver();
         Uri uri = Calendars.CONTENT_URI;
@@ -41,11 +51,27 @@ public class feedback extends PApplet {
                 "sampleuser@gmail.com"};
         // Submit the query and get a Cursor object back.
         cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
+        // Use the cursor to step through the returned records
+        int i = 0;
+        while (cur.moveToNext()) {
+            i++;
+            long calID = 0;
+            String displayName = null;
+            String accountName = null;
+            String ownerName = null;
 
-    }
+            // Get the field values
+            calID = cur.getLong(PROJECTION_ID_INDEX);
+            displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
+            accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
+            ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
 
-    public void draw() {
-        background(255,155,155);
+            text(displayName,0,200*mouseX);
+            text(accountName, 20, 400*mouseX);
+            text(ownerName, 40, 600*mouseX);
+            // Do something with the values...
+
+        }
 
     }
 }
